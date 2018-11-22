3965f7ebfe362715419df3253a61283d7c32775a
Alen Kokotec-Lovrek <alekokote@foi.hr>
2018-11-22 22:27:08 +0100

    ispravak funkcioniranja registracije korisnika (ispravno zapisivanje u bazu, ispravni palatali)

--- app/src/main/java/mpet/project2018/air/mpet/fragments/Registracija.java
+++ app/src/main/java/mpet/project2018/air/mpet/fragments/Registracija.java
@@ -26,6 +26,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import Retrofit.DataPost.RegistracijaMethod;
 import mpet.project2018.air.mpet.R;
 
@@ -51,47 +54,52 @@ public class Registracija extends Fragment {
             mListener.onFragmentInteraction("Registracija korisnika");
         }
 
+        Button buttonSpremi=(Button) view.findViewById(R.id.btnRegistracijaSpremi);
+        Button buttonOdustani=(Button) view.findViewById(R.id.btnRegistracijaOdustani);
+
+        buttonSpremi.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+                                                EditText imeEdit = (EditText)view.findViewById(R.id.unosIme);
+                                                String ime= imeEdit.getText().toString();
+                                                EditText prezimeEdit = (EditText)view.findViewById(R.id.unosPrezime);
+                                                String prezime=prezimeEdit.getText().toString();
+                                                EditText korisnickoEdit = (EditText)view.findViewById(R.id.unosKorIme);
+                                                String korIme=korisnickoEdit.getText().toString();
+                                                EditText adresaEdit = (EditText)view.findViewById(R.id.unosAdresa);
+                                                String adresa=adresaEdit.getText().toString();
+                                                EditText mailEdit = (EditText)view.findViewById(R.id.unosMail);
+                                                String mail=mailEdit.getText().toString();
+                                                EditText telefonEdit = (EditText)view.findViewById(R.id.unosTelefon);
+                                                String telefon=telefonEdit.getText().toString();
+                                                EditText mobitelEdit = (EditText)view.findViewById(R.id.unosMobitel);
+                                                String mobitel=mobitelEdit.getText().toString();
+                                                EditText lozinkaEdit = (EditText)view.findViewById(R.id.unosLozinka);
+                                                String lozinka=lozinkaEdit.getText().toString();
+                                                if(TextUtils.isEmpty(ime)||TextUtils.isEmpty(prezime)||TextUtils.isEmpty(mail)||lozinka.length()<3||korIme.length()<3){
+                                                    Toast.makeText(getActivity(), "Provjerite polja!",
+                                                            Toast.LENGTH_LONG).show();
+                                                }
+                                                else{
+                                                    RegistracijaMethod.Upload(ime, prezime, korIme, adresa, mail, mobitel, telefon, lozinka);
+                                                    swapFragment();
+                                                    Toast.makeText(getActivity(), "Registrirali ste se",
+                                                            Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        }
+        );
+
+        buttonOdustani.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                swapFragment();
+            }
+        });
 
        /* Button buttonSpremi=(Button) getActivity().findViewById(R.id.btnRegistracijaSpremi);
         Button buttonOdustani=(Button) getActivity().findViewById(R.id.btnRegistracijaOdustani);
 
-        buttonSpremi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                EditText imeEdit = (EditText)view.findViewById(R.id.unosIme);
-                String ime=imeEdit.getText().toString();
-                EditText prezimeEdit = (EditText)view.findViewById(R.id.unosPrezime);
-                String prezime=prezimeEdit.getText().toString();
-                EditText korisnickoEdit = (EditText)view.findViewById(R.id.unosKorIme);
-                String korIme=korisnickoEdit.getText().toString();
-                EditText adresaEdit = (EditText)view.findViewById(R.id.unosAdresa);
-                String adresa=adresaEdit.getText().toString();
-                EditText mailEdit = (EditText)view.findViewById(R.id.unosMail);
-                String mail=mailEdit.getText().toString();
-                EditText telefonEdit = (EditText)view.findViewById(R.id.unosTelefon);
-                String telefon=telefonEdit.getText().toString();
-                EditText mobitelEdit = (EditText)view.findViewById(R.id.unosMobitel);
-                String mobitel=mobitelEdit.getText().toString();
-                EditText lozinkaEdit = (EditText)view.findViewById(R.id.unosLozinka);
-                String lozinka=lozinkaEdit.getText().toString();
-                if(TextUtils.isEmpty(ime)||TextUtils.isEmpty(prezime)||TextUtils.isEmpty(mail)||lozinka.length()<3||korIme.length()<3){
-                    //Poruka("Provjerite polja!");
-                }
-                else{
-                    RegistracijaMethod.Upload(ime, prezime, korIme, adresa, mail, mobitel, telefon, lozinka);
-                    //Poruka("Registrirali ste se!");
-
-                }
-
-            }
-        });
-/*
-        buttonOdustani.setOnClickListener(new View.OnClickListener() {
-
-        });
-        */
-/*
         private void Poruka(String tekst){
             Context context = getApplicationContext();
             CharSequence text = tekst;
@@ -101,12 +109,13 @@ public class Registracija extends Fragment {
             toast.show();
         }
         */
+
         return view;
     }
 
     private void swapFragment(){
         FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-        ft.replace(R.id.mainFrame, new Pocetna());
+        ft.replace(R.id.mainFrame, new Pocetna_neulogirani());
         ft.commit();
     }
     @Override
