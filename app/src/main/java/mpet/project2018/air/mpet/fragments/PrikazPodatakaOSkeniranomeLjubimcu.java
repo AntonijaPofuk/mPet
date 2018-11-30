4ff986d7288bdaf97436022934af28b25f785601
Luka Košćak <lkoscak@foi.hr>
2018-11-30 14:57:18 +0100

    Fragment klasa za prikaz podataka o skeniranome ljubimcu - kreirana - potrebno instanciranje polja za prikaz podataka.

---
+++ app/src/main/java/mpet/project2018/air/mpet/fragments/PrikazPodatakaOSkeniranomeLjubimcu.java
@@ -0,0 +1,31 @@
+package mpet.project2018.air.mpet.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import mpet.project2018.air.mpet.R;
+import mpet.project2018.air.nfc.NFCManager;
+
+public class PrikazPodatakaOSkeniranomeLjubimcu extends Fragment implements View.OnClickListener {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.prikaz_podataka_skeniranog_ljubimca,container,false);
+        return  view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View v) {
+    }
+}
