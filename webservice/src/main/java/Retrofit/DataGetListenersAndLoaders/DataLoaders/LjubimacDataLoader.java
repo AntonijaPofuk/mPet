c154ea8f0d84ee9b865cb1e1eb3347299e93264d
Luka Košćak <lkoscak@foi.hr>
2018-12-05 18:35:57 +0100

    Dodani moduli za implementaciju modularnosti.

--- webservice/src/main/java/Retrofit/DataGetListenersAndLoaders/DataLoaders/LjubimacDataLoader.java
+++ webservice/src/main/java/Retrofit/DataGetListenersAndLoaders/DataLoaders/LjubimacDataLoader.java
@@ -1,16 +1,12 @@
 package Retrofit.DataGetListenersAndLoaders.DataLoaders;
 
-import com.raizlabs.android.dbflow.sql.language.SQLite;
-
 import java.util.List;
 
 import Retrofit.DataGet.LjubimacData;
 import Retrofit.DataGetListenersAndLoaders.DataLoadedListeners.LjubimacDataLoadedListener;
 import Retrofit.DataGetListenersAndLoaders.WebServiceHandler;
-import Retrofit.Model.Korisnik;
 import Retrofit.Model.Ljubimac;
-import mpet.project2018.air.database.entities.Korisnik_Table;
-import mpet.project2018.air.database.entities.Ljubimac_Table;
+
 
 
 public class LjubimacDataLoader  {
@@ -40,7 +36,7 @@ public class LjubimacDataLoader  {
         public void onDataArrived(Object result, boolean ok) {
             if(ok){
                 List<Ljubimac> listaLjubimaca = (List<Ljubimac>) result;
-                saveInLocalDatabase(listaLjubimaca);
+                //saveInLocalDatabase(listaLjubimaca);
                 petsArrived = true;
                 checkDataArrival(listaLjubimaca);
             }
@@ -54,25 +50,5 @@ public class LjubimacDataLoader  {
         }
     }
 
-    private void saveInLocalDatabase(List<Ljubimac> listaDohvacenihLjubimaca)
-    {
-        for (Ljubimac ljubimac : listaDohvacenihLjubimaca)
-        {
-            mpet.project2018.air.database.entities.Korisnik dohKorisnik =
-                    SQLite.select().from(mpet.project2018.air.database.entities.Korisnik.class).where(Korisnik_Table.id_korisnika.is(Integer.parseInt(ljubimac.korisnik))).querySingle();
 
-            if(dohKorisnik!=null)
-            {
-                mpet.project2018.air.database.entities.Ljubimac newLjubimac= new mpet.project2018.air.database.entities.Ljubimac(
-                        Integer.parseInt(ljubimac.id_ljubimca),ljubimac.ime,Integer.parseInt(ljubimac.godina),Long.parseLong(ljubimac.masa),ljubimac.vrsta,
-                        ljubimac.spol,ljubimac.opis,ljubimac.url_slike,dohKorisnik);
-            }
-            else
-            {
-                mpet.project2018.air.database.entities.Ljubimac newLjubimac= new mpet.project2018.air.database.entities.Ljubimac(
-                        Integer.parseInt(ljubimac.id_ljubimca),ljubimac.ime,Integer.parseInt(ljubimac.godina),Long.parseLong(ljubimac.masa),ljubimac.vrsta,
-                        ljubimac.spol,ljubimac.opis,ljubimac.url_slike,null);
-            }
-        }
-    }
 }
